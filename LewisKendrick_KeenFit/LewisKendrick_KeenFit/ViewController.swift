f309c84cd5b21ade66451acf224ea8e2a4ec0b97
kendrick lewis <kclewis2@fullsail.edu>
2017-11-02 09:52:50 -0400

    This is a test to see if this commit works

---
+++ LewisKendrick_KeenFit/LewisKendrick_KeenFit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LewisKendrick_KeenFit
+//
+//  Created by kendrick lewis on 10/27/17.
+//  Copyright © 2017 kendrick lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
