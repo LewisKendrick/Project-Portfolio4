d9d787f337200f46ee3473b7fce7ca5ce44210c3
kendrick lewis <kclewis2@fullsail.edu>
2017-11-06 23:55:44 -0500

    I have been trying a ideas out and attempting to log in through firebase

--- LewisKendrick_KeenFit/LewisKendrick_KeenFit/LoginScreen.swift
+++ LewisKendrick_KeenFit/LewisKendrick_KeenFit/LoginScreen.swift
@@ -6,19 +6,44 @@
 //  Copyright © 2017 kendrick lewis. All rights reserved.
 //
 
+
+
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class LoginScreenContoller: UIViewController {
     
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    var ref: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        ref = Database.database().reference()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SignInButton(_ sender: Any)
+    {
+      
+    }
+    
+    @IBAction func SignUpButton(_ sender: Any)
+    {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        
     }
 
 
